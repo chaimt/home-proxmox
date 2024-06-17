6051ac17eae30189b28632b40c4126fef472e2a9
Chaim Turkel <chaim.turkel.external@IL-ChaimTikal.local>
2024-06-17 22:12:50 +0300

    amir20/dozzle:v7.0.6

--- oneuptime/update.sh
+++ oneuptime/update.sh
@@ -35,7 +35,7 @@ services:
 
   dozzle:
     container_name: ${DOCKER_CONTAINER_NAME_PREFIX}_dozzle
-    image: amir20/dozzle:v7.0.5
+    image: amir20/dozzle:v7.0.6
     restart: unless-stopped
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock
