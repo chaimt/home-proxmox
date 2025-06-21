f1348b1f7b80e28b9a04b91019a714b9cd6383ca
Chaim Turkel <cyturel@gmail.com>
2025-06-22 01:39:08 +0300

    logs

--- model-serving/src/main.py
+++ model-serving/src/main.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app, 
         host="0.0.0.0", 
-        port=8000,
+        port=8080,
         limit_concurrency=1000,
         timeout_keep_alive=30
     )
